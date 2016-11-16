9cd7a0eb43883a314dce4414ecc2e897533e9861
xuwuqiang <lockiee@outlook.com>
2016-11-16 14:21:06 +0800

    test3

---
+++ src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xuwuqiang
+ * Date: 2016/11/15
+ * Time: 17:13
+ */
+
+// src/AppBundle/Controller/LuckyController.php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController
+{
+    /**
+     * @Route("/lucky/number")
+     */
+    public function numberAction()
+    {
+        $number = rand(0, 100);
+//        echo $number;exit;
+        return new Response(
+            '<html><body>Lucky number is: '.$number.'</body></html>'
+        );
+    }
+}
